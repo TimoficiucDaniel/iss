cee5298e910c834932186e8f90ad7a39b7264e15
Andrei_Varga <vargandr@yahoo.com>
2023-05-25 17:41:03 +0300

    Update BucketListController.java

--- src/main/java/group/iss/Controller/BucketListController.java
+++ src/main/java/group/iss/Controller/BucketListController.java
@@ -5,6 +5,7 @@ import group.iss.Model.Destination;
 import group.iss.Service.BucketListService;
 import group.iss.Service.DestinationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,17 +21,20 @@ public class BucketListController {
     @Autowired
     DestinationService destinationService;
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/{id}/bucket-list/{page}")
     public List<BucketListItem> getBucketList(@PathVariable Long id,@PathVariable int page){
         return service.getBucketList(page,id);
     }
 
+    @PreAuthorize("hasRole('USER')")
     @PostMapping("/{id}/bucket-list/add")
     public void addDestination(@PathVariable Long id, @RequestBody Long destinationId){
         Destination destination = destinationService.getById(destinationId);
         service.saveDestination(destination, id);
     }
 
+    @PreAuthorize("hasRole('USER')")
     @DeleteMapping("/{id}/bucket-list/delete/{item}")
     public void deleteDestination(@PathVariable Long item){
         service.deleteDestination(item);
